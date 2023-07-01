ef890f45323df9422c3e08448eb021fcf51a9293
Dragos Rachieru <rachierudragos97@gmail.com>
2023-07-01 13:30:10 +0300

    Updated libraries

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,18 +1,18 @@
 object Versions {
   const val kotlin = "1.8.20"
-  const val compose = "1.4.0"
-  const val coroutines = "1.6.4"
-  const val serialization = "1.5.0"
+  const val compose = "1.4.1"
+  const val coroutines = "1.7.2"
+  const val serialization = "1.5.1"
   const val cli = "0.3.5"
-  const val kodein = "7.19.1"
-  const val voyager = "1.0.0-rc04"
+  const val kodein = "7.20.2"
+  const val voyager = "1.0.0-rc06"
   const val exposed = "0.41.1"
   const val sqliteJdbc = "3.41.2.1"
   const val okio = "3.3.0"
 
   const val ktlcnc = "0.0.2"
 
-  const val logging = "1.5.9"
+  const val logging = "4.0.1"
 
   object Grpc {
     const val plugin = "0.9.2"
